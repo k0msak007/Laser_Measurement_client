55d7a76eebf0212b10b564384a0d6bbc23b36940
komsak007 <98792390+k0msak007@users.noreply.github.com>
2024-12-23 10:12:36 +0700

    update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Laser_Measurement_client",
+  base: "/Laser_Measurement_client/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
